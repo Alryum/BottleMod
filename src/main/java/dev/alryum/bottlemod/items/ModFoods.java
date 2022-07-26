cc3d87650f19c5c537926d55a98e1f97a874f3b4
pixelcat2211 <pixelcat2211@gmail.com>
2022-07-26 19:44:50 +0500

    init

---
+++ src/main/java/dev/alryum/bottlemod/items/ModFoods.java
@@ -0,0 +1,9 @@
+package dev.alryum.bottlemod.items;
+
+import net.minecraft.world.food.FoodProperties;
+
+public class ModFoods {
+
+    public static final FoodProperties RESIN_MEAT = (new FoodProperties.Builder()).nutrition(6).saturationMod(0.6F).meat().build();
+
+}
